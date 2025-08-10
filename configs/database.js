534163ecb0e831636c523e214df5a8e70ddd32b9
jobigutenburg02 <jbros2513@gmail.com>
2025-08-10 11:21:18 +0400

    changed MongoDB URL to cluster for production

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-  mongoose.connect(process.env.MONGODB_URI_LOCAL)
+  mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Database Connected'))
     .catch(err => {
       console.log("Database Connection Failed", err);
